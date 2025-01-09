fda513c7e0da3c5e75389110ebeb9367764014eb
miguel <mallquilm@gmail.com>
2025-01-08 22:31:16 -0500

    inyección por propiedad y metodo con anotacion

--- src/main/java/edu/cibertec/capitulo1/AppConfig.java
+++ src/main/java/edu/cibertec/capitulo1/AppConfig.java
@@ -1,7 +1,10 @@
 
 package edu.cibertec.capitulo1;
 
+import edu.cibertec.capitulo1.beans.DocumentoExcel;
+import edu.cibertec.capitulo1.beans.DocumentoTexto;
 import edu.cibertec.capitulo1.beans.HolaMundo;
+import edu.cibertec.capitulo1.service.ImpresoraService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +14,21 @@ public class AppConfig {
     @Bean
     public HolaMundo holaMundoBean(){
         return new HolaMundo();
+    }
+    
+    @Bean
+    public DocumentoExcel documentoExcel(){
+        return new DocumentoExcel();
+    }
+    
+    @Bean
+    public ImpresoraService impresoraService(){
+        return new ImpresoraService();
+    }
+    
+    @Bean
+    public DocumentoTexto documentoTexto(){
+        return new DocumentoTexto();
     }
     
 }
--- src/main/java/edu/cibertec/capitulo1/service/ImpresoraService.java
+++ src/main/java/edu/cibertec/capitulo1/service/ImpresoraService.java
@@ -2,9 +2,13 @@
 package edu.cibertec.capitulo1.service;
 
 import edu.cibertec.capitulo1.beans.Imprimible;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class ImpresoraService {
     
+    @Autowired
+    @Qualifier("documentoTexto")
     private Imprimible documento;
     
     public ImpresoraService(){}
